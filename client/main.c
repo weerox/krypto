34b7f78b59a8eeb8b0fa06058f6387fbd9686861
Aron Bergman <bathingrad@gmail.com>
2019-10-31 13:39:48 +0100

    Create connection between client and server

--- client/main.c
+++ client/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <arpa/inet.h>
+
+#include <netinet/in.h>
+
+#include <sys/socket.h>
+
 // port 31415 is an unassigned port
 // according to the IANA Port Number Registry
 #define DEFAULT_PORT 31415
@@ -35,5 +41,28 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+	if (sockfd == -1) {
+		fprintf(stderr, "Couldn't create socket\n");
+		return 1;
+	}
+
+	struct sockaddr_in address;
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+
+	if (inet_aton("127.0.0.1", &address.sin_addr) == 0) {
+		fprintf(stderr, "Couldn't parse the supplied address\n");
+		return 1;
+	}
+
+	if (connect(sockfd, (struct sockaddr *) &address, sizeof(address))) {
+		fprintf(stderr, "Couldn't create a connection on the socket\n");
+		return 1;
+	}
+
+	close(sockfd);
 	return 0;
 }
--- server/main.c
+++ server/main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <netinet/in.h>
+
+#include <sys/socket.h>
+
 // port 31415 is an unassigned port
 // according to the IANA Port Number Registry
 #define DEFAULT_PORT 31415
@@ -30,5 +34,40 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+	if (sockfd == -1) {
+		printf("Couldn't create socket\n");
+		return 1;
+	}
+
+	struct sockaddr_in address;
+	socklen_t socklen;
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	address.sin_addr.s_addr = INADDR_ANY;
+
+	socklen = sizeof(address);
+
+	if (bind(sockfd, (struct sockaddr *) &address, socklen) == -1) {
+		fprintf(stderr, "Couldn't bind the socket\n");
+		return 1;
+	}
+
+	if (listen(sockfd, 0) == -1) {
+		fprintf(stderr, "Couldn't listen on socket\n");
+		return 1;
+	}
+
+	int accepted_socket =
+		accept(sockfd, (struct sockaddr *) &address, &socklen);
+
+	if (accepted_socket == -1) {
+		fprintf(stderr, "Couldn't accept the connection\n");
+		return 1;
+	}
+
+	close(sockfd);
 	return 0;
 }

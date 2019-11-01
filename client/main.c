cd35bb4e1c475ab2da86f797ad9512a08b59e7e2
Aron Bergman <bathingrad@gmail.com>
2019-11-01 11:48:44 +0100

    Encrypt file

--- client/main.c
+++ client/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <arpa/inet.h>
 
 #include <netinet/in.h>
+
+#include <openssl/evp.h>
+#include <openssl/rand.h>
 
 #include <sys/socket.h>
 
@@ -12,6 +16,90 @@
 // according to the IANA Port Number Registry
 #define DEFAULT_PORT 31415
 #define DEFAULT_IP "127.0.0.1"
+
+// generates a key and an IV from a password
+// the salt used for generation is returned
+unsigned char *generate_key_iv(
+	char *password, unsigned char *key, unsigned char *iv
+) {
+	unsigned char *salt = malloc(8);
+
+	RAND_bytes(salt, 8);
+
+	EVP_BytesToKey(
+		EVP_aes_256_gcm(), EVP_sha256(), (unsigned char *) salt,
+		(unsigned char *) password, strlen(password), 1,
+		key, iv
+	);
+
+	return salt;
+}
+
+// encrypts `filename' using `password'
+// returns the filename of the encrypted file
+char *encrypt_file(char *filename, char *password) {
+	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+
+	if (ctx == NULL) {
+		printf("Couldn't create a new cipher context\n");
+		return 0;
+	}
+
+	if (EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL) == 0) {
+		printf("Couldn't initialize encryption\n");
+		return 0;
+	}
+
+	unsigned char key[EVP_MAX_KEY_LENGTH];
+	unsigned char iv[EVP_MAX_IV_LENGTH];
+
+	unsigned char *salt = generate_key_iv(password, key, iv);
+
+	// the filename of the encoded file is the Base64 encoding of the salt
+	char *filename_enc = malloc(13);
+	EVP_EncodeBlock(filename_enc, salt, 8);
+
+	FILE *fplain = fopen(filename, "r");
+	FILE *fenc = fopen(filename_enc, "w");
+
+	char buffer[1024], buffer_enc[1024];
+	int buffer_len = 0, buffer_enc_len = 0;
+
+	if (EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, key, iv) == 0) {
+		printf("Couldn't initialize encryption\n");
+		return 0;
+	}
+
+	while (!feof(fplain)) {
+		buffer_len = fread(buffer, 1, 1024, fplain);
+
+		if (EVP_EncryptUpdate(
+			ctx,
+			(unsigned char *) buffer_enc, &buffer_enc_len,
+			(unsigned char *) buffer, buffer_len
+		) == 0) {
+			printf("Couldn't encrypt data\n");
+			return 0;
+		}
+
+		fwrite(buffer_enc, 1, buffer_enc_len, fenc);
+	}
+
+	if (EVP_EncryptFinal_ex(
+		ctx,
+		(unsigned char *) buffer_enc, &buffer_enc_len
+	) == 0) {
+		printf("Couldn't encrypt data\n");
+		return 0;
+	}
+
+	fwrite(buffer_enc, 1, buffer_enc_len, fenc);
+
+	fclose(fenc);
+	fclose(fplain);
+
+	return filename_enc;
+}
 
 int create_socket(char *ip, int port) {
 	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
@@ -83,6 +171,7 @@ int main(int argc, char **argv) {
 	char password[256];
 	fgets(password, 256, stdin);
 
+	char *file_enc = encrypt_file(file, password);
 
 	close_socket(sockfd);
 	return 0;
